a28258d23f286c0230293a3c5e8168a98b804b72
Yafreilis <130526666+Yafreilis@users.noreply.github.com>
2025-11-23 23:15:11 -0400

    cambios al form

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,4 +1,3 @@
-// src/components/ProductForm.js
 import React, { useState, useEffect } from 'react';
 
 function ProductForm({ addProduct, updateProduct, editingProduct, setEditingProduct }) {
@@ -41,18 +40,24 @@ function ProductForm({ addProduct, updateProduct, editingProduct, setEditingProd
 
   return (
     <form onSubmit={onSubmit} style={{marginBottom:12}}>
-      <div className="form-row">
-        <input className="stretch" type="text" name="name" placeholder="Nombre del producto" value={formData.name} onChange={handleChange} required />
-        <button className="btn btn-primary" type="submit">{editingProduct ? 'Guardar' : 'Añadir'}</button>
-      </div>
-
       <div className="form-grid">
-        <input type="text" name="category" placeholder="Categoría" value={formData.category} onChange={handleChange} required />
-        <input type="number" name="price" placeholder="Precio" step="0.01" value={formData.price} onChange={handleChange} required />
-
-        <input type="number" name="stock" placeholder="Stock" value={formData.stock} onChange={handleChange} required />
-        <div style={{display:'flex', alignItems:'center', gap:10}}>
-          {editingProduct && <button type="button" className="btn btn-secondary" onClick={onCancel}>Cancelar</button>}
+        <div className='col-span-4'>
+          <input  type="text" name="name" placeholder="Nombre del producto" value={formData.name} onChange={handleChange} required />
+        </div>
+        <div className='col-span-2'>
+          <button className="btn btn-primary" type="submit">{editingProduct ? 'Guardar' : 'Añadir'}</button>
+        </div>
+        <div className='col-span-2'>
+          <input type="text" name="category" placeholder="Categoría" value={formData.category} onChange={handleChange} required />
+        </div>
+        <div className='col-span-2'>
+          <input type="number" name="price" placeholder="Precio" step="0.01" value={formData.price} onChange={handleChange} required />
+        </div>
+        <div className='col-span-2'>
+          <input type="number" name="stock" placeholder="Stock" value={formData.stock} onChange={handleChange} required />
+        </div>
+        <div className="col-span-6" style={{display:'flex', alignItems:'center', justifyContent:"flex-end", gap:10}}>
+          {editingProduct && <button type="button" className="btn btn-secondary" style={{width:"auto"}} onClick={onCancel}>Cancelar</button>}
         </div>
       </div>
     </form>
